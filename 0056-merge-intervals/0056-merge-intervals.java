deb96b4e5134090da7ea4ca4f099f3f0aabdc149
Suyash <suyashchamoli1@gmail.com>
2026-05-22 22:49:46 +0530

    Time: 7 ms (98.93%), Space: 49.3 MB (24.34%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,63 +1,23 @@
-import java.util.*;
-
-
 class Solution {
-    public int[][] merge(int[][] m) {
-        int r= m.length;
-        if(r==1){
-            return m;
+    public int[][] merge(int[][] mat) {
+        int n= mat.length;
+        Arrays.sort(mat, (a,b)->a[0]-b[0]);
+        int i=0;
+        List<int[]> al= new ArrayList<>();
 
-        }
-        pairs[] p= new pairs[r];
-        for(int i=0;i<r;i++){
-            p[i]= new pairs(m[i][0],m[i][1]);
-        }
-        Arrays.sort(p);
-        //merging
-        Stack<pairs> st= new Stack<>();
-        st.push(p[0]);
-        for(int i=0;i<r;i++){
-            pairs top= st.peek();
-
-            if(p[i].st>top.et){
-                st.push(p[i]);
-            }
-            else{
-                top.et= Math.max(p[i].et,top.et);
-            }
-        }
-
-       int[][] res= new int[st.size()][2];
-        for(int i=st.size()-1;i>=0;i--){
-            pairs k= st.pop();
-            
-            res[i][0]= k.st;
-            res[i][1]= k.et;
-        }
-
-   return res;
-
-
-    }
-    class pairs implements Comparable<pairs>{
-        int st;
-        int et;
-        pairs(int st,int et){
-            this.st=st;
-            this.et=et;
-
-        }
-
-
-        @Override
-        public int compareTo(pairs o) {
-          if(this.st!=o.st){
-              return this.st-o.st;
+        while(i<n){
+          int min= mat[i][0];
+          int max= mat[i][1];
+          while(i<n-1 && mat[i+1][0]<= max){
+           i++;
+            max= Math.max(max,mat[i][ 1]);
           }
-          else{
-              return this.et-o.et;
-          }
+          al.add(new int [] {min, max});
+          i++;
         }
-    }
 
+int [][] res= al.toArray(new int [al.size()][]);
+return res;
+        
+    }
 }
