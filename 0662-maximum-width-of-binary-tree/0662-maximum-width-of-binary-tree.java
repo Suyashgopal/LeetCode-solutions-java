7101a770c064ec771cf1f01f6e794ba40be39472
Suyash <suyashchamoli1@gmail.com>
2026-05-25 21:07:20 +0530

    Time: 1 ms (99.51%), Space: 45.3 MB (31.33%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -2,9 +2,9 @@ class Solution {
 
     class pair {
         TreeNode node;
-        long id;
+        int id;
 
-        pair(TreeNode node, long id) {
+        pair(TreeNode node, int id) {
             this.node = node;
             this.id = id;
         }
@@ -21,35 +21,29 @@ class Solution {
         q.add(new pair(root, 0));
 
         while (!q.isEmpty()) {
-
-            int size = q.size();
-
-            long minid = q.peek().id;
-
-            long first = 0;
-            long last = 0;
-
-            for (int i = 0; i < size; i++) {
-
-                pair curr = q.remove();
-
-                TreeNode node = curr.node;
-
-                long id = curr.id - minid;
-
-                if (i == 0) first = id;
-                if (i == size - 1) last = id;
-
-                if (node.left != null) {
-                    q.add(new pair(node.left, 2 * id));
-                }
-
-                if (node.right != null) {
-                    q.add(new pair(node.right, 2 * id + 1));
-                }
+         int size= q.size();
+         int first=0;
+         int last=0;
+         for(int i=0;i<size;i++){
+            pair curr= q.poll();
+            TreeNode node= curr.node;
+            int id= curr.id;
+            if(i==0){
+            first=id;
             }
+            if(i== size-1){
+                last=id;
+            }
+            if(node.left!= null){
+                q.add(new pair(node.left, 2*id));
+            }
+            if(node.right!= null){
+                q.add(new pair(node.right,2*id+1));
+            }
+         }
+           maximum= Math.max(maximum, last- first+1);
 
-            maximum = Math.max(maximum, (int)(last - first + 1));
+          
         }
 
         return maximum;
