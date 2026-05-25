0cc2483044f418f7ea76b28b43b2047cc010c8fe
Suyash <suyashchamoli1@gmail.com>
2026-05-25 19:47:58 +0530

    Time: 7 ms (98.9%), Space: 46.7 MB (10.97%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -7,85 +7,26 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
 class Solution {
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-        ArrayList<TreeNode> a1 = new ArrayList<>();
-        ArrayList<TreeNode> a2 = new ArrayList<>();
-
-        givepath(a1, p.val, root);
-        givepath(a2, q.val, root);
-
-        // getting the first common ancestor
-
-        if (a1.contains(q)) {
-            return q;
-        }
-
-        if (a2.contains(p)) {
-            return p;
-        }
-
-        int i = a1.size() - 1;
-        int j = a2.size() - 1;
-
-        while (i >= 0 && j >= 0) {
-
-            if (a1.contains(a2.get(j))) {
-                return a2.get(j);
-            }
-
-            if (a2.contains(a1.get(i))) {
-                return a1.get(i);
-            }
-
-            i--;
-            j--;
-        }
-
-        while (i >= 0) {
-
-            if (a2.contains(a1.get(i))) {
-                return a1.get(i);
-            }
-
-            i--;
-        }
-
-        while (j >= 0) {
-
-            if (a1.contains(a2.get(j))) {
-                return a2.get(j);
-            }
-
-            j--;
-        }
-
-        return root;
+        
+     return    lca(root,p,q);
     }
+    TreeNode lca(TreeNode root, TreeNode p, TreeNode q){
 
-    boolean givepath(ArrayList<TreeNode> al, int target, TreeNode node) {
-
-        if (node == null) {
-            return false;
+        if(root==null| root==p|| root==q){
+            return root;
         }
-
-        al.add(node);
-
-        if (node.val == target) {
-            return true;
+        TreeNode l= lca(root.left, p,q);
+        TreeNode r= lca(root.right,p,q);
+        if(l== null){
+            return r;
         }
-
-        if (givepath(al, target, node.left) ||
-            givepath(al, target, node.right)) {
-
-            return true;
+        else if(r== null){
+            return l;
         }
-
-        al.remove(al.size() - 1);
-
-        return false;
+        else{
+            return root;
+        }
     }
 }
