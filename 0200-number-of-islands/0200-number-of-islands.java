9e43645ff63550c89f8660d580e734a833237788
Suyash <suyashchamoli1@gmail.com>
2026-05-30 21:56:50 +0530

    Time: 12 ms (5.47%), Space: 52.3 MB (46.36%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,66 +1,52 @@
+
+
+
+
 class Solution {
+
     public int numIslands(char[][] mat) {
-        //convert mat to adj list
-        List<List<Integer>> adj= new ArrayList<>();
         int row= mat.length;
         int col= mat[0].length;
-        
+        boolean[][] visit= new boolean[row][col];
+        int counter=0;
 
-
-            for(int i=0;i< (row* col) ;i++){
-            adj.add(new ArrayList<Integer>());
-             }
-             int[] visit= new int[adj.size()+1];
-             Arrays.fill(visit, 0);
-
-      for(int i=0;i<row;i++){
-       for(int j=0;j< col;j++){
-        if(mat[i][j]=='1'){
-            int num= i* col + j;
-            visit[num]=1;
-            adj.get(num).add(num);
-            //its upper one
-            if(i>0 && mat[i-1][j]=='1'){
-             adj.get(num).add((i-1)*col+(j));
-            }
-
-            //its lower one
-            if(i<row-1 && mat[i+1][j]=='1'){
-             adj.get(num).add((i+1)*col+(j));
-            }
-
-            //its left one
-            if(j>0 && mat[i][j-1]=='1'){
-             adj.get(num).add((i)*col+(j-1));
-            }
-
-            //right one
-             if(j<col-1 && mat[i][j+1]=='1'){
-             adj.get(num).add((i)*col+(j+1));
-            }}
-
-
-       }
-      }
-     int count=0;
-      for(int i=0;i<visit.length ;i++)
-      {  if(visit[i]!=0){
-          dfs(i,adj, visit);
-          count++;
-      }
-
-      }
-        
-        return count;
-        
-    }
-    void dfs( int i,List<List<Integer>> adj,int [] visit ){
-        visit[i]=0;
-        for(int k : adj.get(i)){
-            if(visit[k]!=0){
-            dfs(k, adj, visit);
-
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(mat[i][j]=='1' && !visit[i][j]){
+                    counter++;
+                    bfs(i,j,mat,visit);
+                }
             }
         }
+
+
+         return counter;   
+
+
     }
-}
+    void bfs(int i, int j, char[][] mat, boolean[][] visit){
+        int row= mat.length;
+        int col= mat[0].length;
+           Queue<ArrayList<Integer>> q = new LinkedList<>();
+           visit[i][j]= true;
+        q.offer(new ArrayList<>(Arrays.asList(i, j)));
+        while(!q.isEmpty()){ 
+           ArrayList<Integer> al= q.poll();
+           i= al.get(0);
+           j= al.get(1);
+           for(int delrow=-1; delrow<=1;delrow++){
+            for(int delcol=-1; delcol<=1 ; delcol++){
+if(Math.abs(delrow) + Math.abs(delcol) != 1) continue;
+                int nrow= i+delrow;
+                int ncol= j+ delcol;
+             if(nrow>=0 && nrow<row && ncol>=0 && ncol<col && mat[nrow][ncol]=='1' && !visit[nrow][ncol]){
+                visit[nrow][ncol]= true;
+            q.offer(new ArrayList<>(Arrays.asList(nrow, ncol)));
+              }
+            }
+           }
+ }
+
+    }}
+    
+    
