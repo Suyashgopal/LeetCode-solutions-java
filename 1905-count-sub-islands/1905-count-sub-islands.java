4a6333b9b11474964a03f977966ad8d72c4461b1
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-06-09 16:15:01 +0530

    Time: 44 ms (6.81%), Space: 121.2 MB (24.56%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -2,21 +2,14 @@ class Solution {
     public int countSubIslands(int[][] mat1, int[][] mat2) {
         int row= mat1.length;
         int col= mat1[0].length;
-        boolean[][] visit1= new boolean[row][col];
-        boolean[][] visit2= new boolean[row][col];
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(mat1[i][j]==1 && !visit1[i][j]){
-                    bfs(i,j, mat1, visit1);
-                }
-            }
-        }
 
-     int count=0;
+        boolean[][] visit= new boolean[row][col];
+        
+        int count=0;
      for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
-            if(mat2[i][j]==1 && !visit2[i][j]){
-              boolean isset=  bfs(i,j,mat2,visit1, visit2);
+            if( mat2[i][j]==1 && !visit[i][j]){
+              boolean isset=  bfs(i,j,mat1,mat2,visit);
               if(isset){count++;}
             }
         }
@@ -28,34 +21,12 @@ class Solution {
 
         
     }
-    void bfs(int i, int j, int[][] mat, boolean[][] visit){
-        int row= mat.length;
-        int col= mat[0].length;
-        Queue<int[]> q= new LinkedList<>();
-        q.add(new int[]{i, j});
-        while(!q.isEmpty()){
-            int[] curr= q.poll();
-            int ci= curr[0];
-            int cj= curr[1];
-            if(visit[ci][cj]){continue;}
-            visit[ci][cj]= true;
-            for(int di= -1; di<=1; di++){
-                for(int dj=-1; dj<=1;dj++){
-                    if(Math.abs(di)+ Math.abs(dj)!=1){continue;}
-                    int ni= ci+ di;
-                    int nj= cj + dj;
-                    if(ni>=0 && ni< row && nj>=0 && nj<col && !visit[ni][nj] && mat[ni][nj]==1){
-                        q.add(new int[]{ni,nj});
-                    }
-                }
-            }
-        }
-    }
+  
 
 
-    boolean bfs(int i, int j, int[][] mat, boolean[][] visit1, boolean[][] visit2){
-        int row= mat.length;
-        int col= mat[0].length;
+    boolean bfs(int i, int j, int[][] mat1,int[][] mat2, boolean[][] visit){
+        int row= mat1.length;
+        int col= mat1[0].length;
         boolean isset= true;
         Queue<int[]> q= new LinkedList<>();
         q.add(new int[]{i, j});
@@ -63,15 +34,15 @@ class Solution {
             int[] curr= q.poll();
             int ci= curr[0];
             int cj= curr[1];
-            if(!visit1[ci][cj]){ isset= false;}
-            if(visit2[ci][cj]){continue;}
-            visit2[ci][cj]= true;
+            if(mat1[ci][cj]==0){ isset= false;}
+            if(visit[ci][cj]){continue;}
+            visit[ci][cj]= true;
             for(int di= -1; di<=1; di++){
                 for(int dj=-1; dj<=1;dj++){
                     if(Math.abs(di)+ Math.abs(dj)!=1){continue;}
                     int ni= ci+ di;
                     int nj= cj + dj;
-                    if(ni>=0 && ni< row && nj>=0 && nj<col && !visit2[ni][nj] && mat[ni][nj]==1){
+                    if(ni>=0 && ni< row && nj>=0 && nj<col && !visit[ni][nj] && mat2[ni][nj]==1){
                         q.add(new int[]{ni,nj});
                     }
                 }
