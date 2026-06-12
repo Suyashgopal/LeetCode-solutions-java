612cb2231174cc8b4353ea166eff8143ae1f41ba
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-06-12 22:46:18 +0530

    Time: 34 ms (23.5%), Space: 47.1 MB (19.17%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,76 +1,68 @@
 class dsu{
     List<Integer> rank= new ArrayList<>();
-    List<Integer> parent= new ArrayList<>();
-    public dsu(int n){
-        for(int i=0;i<n;i++){
-            rank.add(0);
-            parent.add(i);
-
+        List<Integer> parent= new ArrayList<>();
+         dsu(int n){
+            for(int i=0;i<n;i++){
+                parent.add(i);
+                rank.add(0);
+            }
         }
-    }
-    
-    int findparent(int node){
-        if(node == parent.get(node)){
-            return node;
+
+
+        int findparent(int node){
+            if(parent.get(node)==node){
+                return node;
+            }
+            int fp= findparent(parent.get(node));
+            return fp;
         }
-        int fp= findparent(parent.get(node));
-        parent.set(node, fp);
-        return parent.get(node);
+        void union(int u, int v){
+            int fpu= findparent(u);
+            int fpv= findparent(v);
+            if(rank.get(fpu)> rank.get(fpv)){
+                parent.set(fpv, fpu);
 
+            }
+            else if(rank.get(fpv)>rank.get(fpu)){
+                parent.set(fpu,fpv);
+            }
+            else if(rank.get(fpv)==rank.get(fpu)){
+                parent.set(fpv, fpu);
+                int ranku= rank.get(fpu);
+                rank.set(fpu, ranku+1);
 
-    }
-
-    void union( int u, int v){
-        int fpu= findparent(u);
-        int fpv= findparent(v);
-        if(fpu== fpv){
-            return;
+            }
         }
-        else if(rank.get(fpu)> rank.get(fpv)){
-            parent.set(fpv, fpu);
-        }
-         else if(rank.get(fpu)< rank.get(fpv)){
-            parent.set(fpu, fpv);
-        }
-         else {
-               parent.set(fpv, fpu);
-            int ranku= rank.get(fpu);
-            rank.set(fpu, ranku+1);
-
-         
-        }
-    }
-
-
-
-
-
 
 }
 
 
+
+
 class Solution {
-    public int removeStones(int[][] stones) {
-        int n= stones.length;
+    public int removeStones(int[][] mat) {
+        int n= mat.length;
+
         dsu dsu1= new dsu(n);
 
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                if(stones[i][0]== stones[j][0] || stones[i][1]== stones[j][1]){
-                    dsu1.union(i, j);
+                if(mat[i][0]==mat[j][0] || mat[i][1]== mat[j][1]){
+                    dsu1.union(i,j);
+
                 }
             }
         }
 
 
-  int grp=0;
+        int grps=0;
         for(int i=0;i<n;i++){
-            int pi= dsu1.parent.get(i);
-            if(i==pi){
-             grp ++;
+            if(dsu1.findparent(i)==i){
+                grps++;
             }
         }
-        return n-grp;
+        return n-grps;
+
         
     }
 }
