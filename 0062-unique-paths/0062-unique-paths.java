41d761dcb21fd4e727e9ba18b09142b7985376ff
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-06-27 19:51:27 +0530

    Time: 0 ms (100%), Space: 41.9 MB (81.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,33 +1,18 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        if(m==1 && n==1){
-            return 1;
-        }
-        
-        int [][] dp= new int[m][n];
-        for(int[] k: dp){
-            Arrays.fill(k, -1);
+        int [][] dp= new int [m][n];
+        for(int i=0;i< m; i++){
+            dp[i][0]=1;
 
         }
-        for(int i=0; i<m; i++){
-            dp[i][0]=1;
-        }
-        for(int i=0;i<n;i++){
+        for(int i=0;i< n;i++){
             dp[0][i]=1;
         }
-        return fun(m-1, n-1, dp);
-
-
-        
-    }
-    int fun(int r, int c, int [][] dp){
-        if(r==0 && c==0){
-            return 0;
+        for(int i=1;i< m;i++){
+            for(int j=1; j<n;j++){
+                dp[i][j]= dp[i-1][j]+ dp[i][j-1];
+            }
         }
-        if(dp[r][c]!=-1){
-            return dp[r][c];
-        }
-        dp[r][c]= fun(r-1, c, dp) + fun(r, c-1, dp);
-        return dp[r][c];
+      return dp[m-1][n-1];  
     }
 }
