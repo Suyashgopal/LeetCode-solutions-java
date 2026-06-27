11bd6c02af58ab4442eec1b4133c25d3da6835ad
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-06-27 12:30:25 +0530

    Time: 0 ms (100%), Space: 42.8 MB (55.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,36 +1,35 @@
 class Solution {
     public int rob(int[] nums) {
 
-        int n= nums.length;
-        int [] dp= new int[n];
-        Arrays.fill(dp,-1);
-        dp[0]= nums[0];
-        int a= fun(n-1, nums,dp);
-        int b= fun(n-2, nums,dp);
+  int n= nums.length;
+  int [] dp= new int [n];
+  Arrays.fill(dp, -1);
+  dp[0]= nums[0];
+
+   
+ int a=   fun(n-1, nums, dp);
+ int b= fun(n-2, nums, dp);
         return Math.max(a,b);
-
-        
     }
-    int fun(int idx, int[] nums,int[] dp){
-        if(idx<0){
-            return 0;
-        }
-      else  if(idx==0){
-             
-            return nums[idx];
-        }
+   
+    int fun(int idx, int[] nums, int dp[]){
+        if(idx<0){return 0;}
+      
+       if(idx==0){
+      
+         return dp[idx];} 
+     
+       if( dp[idx]!=-1){return dp[idx];}
+       
+      int  left = fun(idx-2, nums, dp)+ nums[idx];
+       
 
-        if(dp[idx]!=-1){return dp[idx];}
+       
+       int  right = fun(idx-3, nums, dp)+ nums[idx];
+       
 
-
-        int left= fun(idx-2, nums,dp)+ nums[idx];
-        int right= fun(idx-3, nums,dp)+ nums[idx];
-
-
-        dp[idx]= Math.max(left, right);
-        return dp[idx];
-
-
+       dp[idx]= Math.max(left, right);
+       return dp[idx];
+       
     }
-
 }
