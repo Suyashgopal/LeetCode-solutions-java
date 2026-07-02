d22b57b677b2b133be3cfdea430bf25a94b467e4
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-07-03 00:23:08 +0530

    Time: 1 ms (20.02%), Space: 46.7 MB (17.94%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,32 +1,32 @@
 class Solution {
     public int maxProfit(int[] arr) {
         int n= arr.length;
-        int[][] dp = new int[n+1][2];
-        for(int[] k: dp){
-            Arrays.fill(k, 0);
-        }
+        
+
+        int[] ahead= new int[2];
+        int[] curr= new int[2];
+        ahead[0]= ahead[1]= 0;
+        
         
         for(int i=n-1;i>=0;i--){
             for(int j=0;j<2;j++){
 
-
+              int profit=0;
                 if(j==1){
-            int a = -arr[i]+ dp[i+1][0] ;
-            int b= dp[i+1][1];
-            dp[i][1]= Math.max(a,b); 
+            
+            profit= Math.max(-arr[i]+ ahead[0],ahead[1]); 
         }
 
          if(j==0){
-            int a= arr[i]+ dp[i+1][1];
-            int b= dp[i+1][0];
-            dp[i][0]= Math.max(a,b);
+          
+            profit= Math.max(arr[i]+ ahead[1] , ahead[0] );
+        }
+        curr[j]= profit;}
+
+        ahead= curr.clone();
         }
 
-
-            }
-        }
-
- return dp[0][1];
+ return curr[1];
 
 
 
