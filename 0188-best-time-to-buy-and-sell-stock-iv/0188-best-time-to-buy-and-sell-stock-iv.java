357d404c857e31400e967d3198fac28e9a2df124
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-07-03 17:14:13 +0530

    Time: 2 ms (71.75%), Space: 45.6 MB (23.32%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -2,33 +2,34 @@ class Solution {
     public int maxProfit(int k, int[] arr) {
         int n= arr.length;
 
-        int[][][] dp= new int[n][2][k+1];
-        for(int[][] h: dp){
-            for(int[] l: h){
-             Arrays.fill(l, -1);
+        int[][][] dp= new int[n+1][2][k+1];
+      
+
+        for(int id= n-1;id>=0;id--){
+            for(int buy=0; buy<2; buy++){
+                for(int t=k-1 ;t>=0;t--){
+                    int pbuy=0;
+                    int psell=0;
+
+                    if(buy==1){
+                     pbuy= Math.max((-arr[id]+ dp[id+1][0][t]) , dp[id+1][1][t]  );
+                    }
+                    else{
+                        psell= Math.max((arr[id]+ dp[id+1][1][t+1]), dp[id+1][0][t]);
+                    }
+                    dp[id][buy][t]= Math.max(pbuy, psell);
+                }
             }
+
+
         }
-        return fun(0,1,k, arr, dp);
+
+
+
+
+
+
+        return dp[0][1][0];
         
-    }
-    int fun(int id,int buy, int t, int[] arr, int[][][]dp){
-        if(t==0|| id==arr.length){
-            return 0;
-        }
-        if(dp[id][buy][t]!=-1){
-            return dp[id][buy][t];
-
-        }
-        int pbuy=0;
-        int psell=0;
-
-        if(buy==1){
-      pbuy= Math.max((-arr[id]+ fun(id+1, 0, t, arr, dp)),   fun(id+1, 1, t,arr, dp));
-        }
-        if(buy==0 && t>0){
-       psell= Math.max((arr[id]+ fun(id+1, 1, t-1, arr, dp)), fun(id+1, 0, t, arr, dp));
-        }
-        dp[id][buy][t]= Math.max(pbuy, psell);
-         return dp[id][buy][t];
     }
 }
