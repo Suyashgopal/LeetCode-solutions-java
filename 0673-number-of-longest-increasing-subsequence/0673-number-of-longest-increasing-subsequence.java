1c0f57564733513c987e7febd89ff7337f6f0975
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-07-07 17:15:40 +0530

    Time: 21 ms (40.68%), Space: 45.5 MB (58.37%) - LeetHub

---
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int findNumberOfLIS(int[] arr) {
+        int n= arr.length;
+
+        int[] dp = new int[n];
+        int[] ways = new int[n];
+        int maxdp=0;
+        int maxid=0;
+
+        Arrays.fill(dp, 1);
+        Arrays.fill(ways, 1);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[j]<arr[i]){
+
+                    if(dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        ways[i] = ways[j];
+                        }
+                  else  if(dp[j]+1== dp[i]){
+                        ways[i]=ways[i]+ ways[j];
+                    }
+
+                    
+                }
+            }
+        }
+
+int maxLen = 0;
+
+for(int len : dp)
+    maxLen = Math.max(maxLen, len);
+
+int ans = 0;
+for(int i = 0; i < n; i++) {
+    if(dp[i] == maxLen)
+        ans += ways[i];
+}
+return ans;    }
+}
