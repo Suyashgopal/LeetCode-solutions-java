9b484f057e57d0c18c35a9c156673228bdd2d8b7
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-07-12 20:49:27 +0530

    Time: 1 ms (100%), Space: 71.4 MB (37.59%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,19 +1,17 @@
 class Solution {
     public int firstMissingPositive(int[] arr) {
-        int n= arr.length;
-        int max=0;
-     HashSet<Integer> set= new HashSet<>();
-     for(int i=0;i<n;i++){
-    set.add(arr[i]);
-    max= Math.max(max,arr[i]);
-     }
-     
-
-     for(int i=1;i<max;i++){
-        if(!set.contains(i)){
+    int n= arr.length;
+    boolean[] seen= new boolean[n+1];
+    for(int i=0;i<n;i++){
+        if(arr[i]>0 && arr[i]<=n){
+            seen[arr[i]]= true;
+        }
+    }
+      for(int i=1;i<=n;i++){
+        if(seen[i]==false){
             return i;
         }
-     }
-     return max+1;
+      }  
+      return n+1;
     }
 }
