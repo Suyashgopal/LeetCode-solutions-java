82f091f2b0bd98399a4a7684513f0e065ebab3d5
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-07-21 12:58:57 +0530

    Time: 2 ms (34.83%), Space: 45.9 MB (9.9%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,27 +1,35 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        ArrayList<Integer> al = new ArrayList<>();
-        List<List<Integer>> outer = new ArrayList<>();
-if(nums.length==0){
-    return outer;
-}
-      fun(al,outer,nums);
-      return outer;
+    public List<List<Integer>> permute(int[] arr) {
+        List<List<Integer>> outer= new ArrayList<>();
+        List<Integer> al= new ArrayList<>();
+        HashSet<Integer> set= new HashSet<>();
+
+        fun(0, arr,al,set, outer);
+        return outer;
+
+        
+
+
+        
     }
-    void fun(  ArrayList<Integer> al,List<List<Integer>> outer,int [] nums ){
-        if(al.size()== nums.length){
-            outer.add(new ArrayList<> (al));
+
+    void fun(int id,int[] arr,List<Integer> al,HashSet<Integer> set, List<List<Integer>> outer ){
+        int n= arr.length;
+
+        if(id==n){
+            outer.add(new ArrayList<>(al));
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(al.contains(nums[i])){
-                continue;
+   
+
+        for(int i=0;i<n;i++){
+            if(!set.contains(arr[i])){
+                set.add(arr[i]);
+                al.add(arr[i]);
+                fun(id+1,arr, al,set,outer);
+                al.remove(al.size()-1);
+                set.remove(arr[i]);
             }
-            al.add(nums[i]);
-            fun(al,outer,nums);
-            al.remove(al.size()-1);
-
         }
-
     }
 }
