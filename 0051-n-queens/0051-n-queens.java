8e39c596950251ffd89465020235fc8f9f6fc448
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-07-21 15:34:13 +0530

    Time: 6 ms (14.68%), Space: 47.2 MB (5.38%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,73 +1,64 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
+        //hashset of rows, cols, d1, d2
+
+        HashSet<Integer> row = new HashSet<>();
+        HashSet<Integer> col = new HashSet<>();
+        HashSet<Integer> d1 = new HashSet<>();
+        HashSet<Integer> d2 = new HashSet<>();
         List<List<String>> outer= new ArrayList<>();
-char[][] b= new char[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-              b[i][j]='.';
-              }
-     }
-        fun(b, outer,0,n);
+        char[][] board= new char[n][n];
+        for(char[] k: board){
+            Arrays.fill(k,'.');
+        }
+
+        fun(0,n,board,row,col,d1,d2,outer);
         return outer;
+
+
 
     }
 
-    void fun(char[][] b ,List<List<String>> outer, int row, int n){
-        if(row==n){
-            //convert the char array to string list
-            //add to outer
-       ArrayList<String> list = new ArrayList<>();
+    void fun(int r, int n, char[][] board, HashSet<Integer> row, HashSet<Integer> col, HashSet<Integer> d1,
+            HashSet<Integer> d2, List<List<String>> outer) {
+        if (r == n) {
 
-            for(int i = 0; i < n; i++){
-    list.add(new String(b[i]));
-}
-   outer.add(new ArrayList<>(list));
-      
+            /// convert the char array to string
+            ArrayList<String> list = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                list.add(new String(board[i]));
+            }
+            outer.add(new ArrayList<>(list));
+
             return;
 
         }
 
-        for(int col= 0;col<n;col++){
-            if(valid(b,row,col,n)){
-              b[row][col]='Q';
-              fun(b,outer,row+1,n);
-              b[row][col]='.';
-            }
-        }
-    }
+            for (int j = 0; j < n; j++) {
 
-    boolean valid(char[][] b, int row, int col, int n){
+                int diag1 = r - j;
+                int diag2 = r + j;
 
-        if(row==0){
-            return true;
-        }
-           //col check 
-            for(int i=0;i<row;i++){
-                if(b[i][col]=='Q'){
-                    return false;
+                if ((!row.contains(r)) && (!col.contains(j)) && (!d1.contains(diag1)) && (!d2.contains(diag2))) {
+                    board[r][j] = 'Q';
+                    row.add(r);
+                    col.add(j);
+                    d1.add(diag1);
+                    d2.add(diag2);
+
+                    fun(r+1,n, board,row, col,d1, d2, outer);
+
+                    //remove
+                    board[r][j] = '.';
+                    row.remove(r);
+                    col.remove(j);
+                    d1.remove(diag1);
+                    d2.remove(diag2);
                 }
+
             }
-            //left dig check
-           int ldr=row;
-           int rdc= col;
-           while(ldr>=0 && rdc>=0){
-            if(b[ldr][rdc]=='Q'){
-                return false;
-            }
-            ldr--;
-            rdc--;
-           }
-           //right dig check
-           int drow= row;
-           int dcol= col;
-           while(drow>=0 && dcol<n){
-            if(b[drow][dcol]=='Q'){
-                return false;
-            }
-            drow--;
-            dcol++;
-           }
-     return true;
+        
 
     }
 }
