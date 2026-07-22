ea6e62e6c34027e24702bc9c5cba9027e8c56724
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-07-22 12:35:39 +0530

    Time: 1 ms (100%), Space: 47.9 MB (60.21%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,52 +1,35 @@
 class Solution {
-    public int[] singleNumber(int[] nums) {
+    public int[] singleNumber(int[] arr) {
+        int n= arr.length;
 
-        // Step 1: XOR all numbers
-        // duplicates cancel, leaving unique1 ^ unique2
-        int xor = 0;
-
-        for(int i = 0; i < nums.length; i++){
-            xor = xor ^ nums[i];
+        //full xor
+        int num=0;
+        for(int i=0;i<n;i++){
+            num= num^ arr[i];
         }
 
+        //get the last set bit
+        int num1= num & (-num);
 
-        // Step 2:
-        // Get rightmost set bit where the two unique numbers differ
-        int mask = xor & (-xor);
+        int lastsetbit=  num1;
 
+        //bucket 1 for nums with ith set bit and 2 for not set bits
 
-        // Step 3:
-        // These will store the two answers
-        int firstUnique = 0;
-        int secondUnique = 0;
+        int bucket1=0;
+        int bucket2=0;
 
+        for(int i=0;i<n;i++){
+            int k= arr[i] & lastsetbit;
 
-        // Step 4:
-        // Split numbers into two groups using that bit
-        for(int i = 0; i < nums.length; i++){
-
-            // Check whether this bit is OFF
-            if( (nums[i] & mask) == 0 ){
-
-                // Goes in first group
-                firstUnique = firstUnique ^ nums[i];
-
+            if(k==0){
+                bucket2= bucket2^arr[i];
             }
             else{
-
-                // Goes in second group
-                secondUnique = secondUnique ^ nums[i];
-
+                bucket1= bucket1^ arr[i];
             }
         }
 
-
-        // Step 5:
-        // return both single numbers
-        int[] result = new int[2];
-        result[0] = firstUnique;
-        result[1] = secondUnique;
-
-        return result;
+     return new int[] { bucket1, bucket2};
+        
     }
 }
