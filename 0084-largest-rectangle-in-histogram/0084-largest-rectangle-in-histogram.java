5d85ebf6bc1748f11478d25a61d5279da6c60780
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-08-02 15:22:43 +0530

    Time: 110 ms (16.5%), Space: 76.2 MB (88.08%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,66 +1,52 @@
 class Solution {
-    public int largestRectangleArea(int[] h) {
-        
-        int n= h.length;
-        
-        //right
+    public int largestRectangleArea(int[] arr) {
+        int n= arr.length;
         Stack<Integer> st= new Stack<>();
-        int i=n-1;
-        int [] right = new int[n];
+        int[] right= new int[n];
+        int i= n-1;
         while(i>=0){
-            while(!st.isEmpty() && h[st.peek()]>=h[i]){
+            while(!st.isEmpty() && arr[i]<=arr[st.peek()]){
                 st.pop();
             }
             if(st.isEmpty()){
-                right[i]= n-i;
+                right[i]= n- i;
                 st.push(i);
             }
-            else if(!st.isEmpty() && h[st.peek()]<h[i]){
-               
-                right[i]=st.peek()-i;
+            else{
+                
+                right[i]= st.peek()-i;
                  st.push(i);
-
             }
-            i--;
-
+         i--;
 
         }
-
-
-        //left
-            Stack<Integer> st1= new Stack<>();
-         i=0;
-        int [] left = new int[n];
-
+        st.clear();
+        i=0;
+        int[] left= new int[n];
         while(i<n){
-            while(!st1.isEmpty() && h[st1.peek()]>=h[i]){
-                st1.pop();
+                   while(!st.isEmpty() && arr[i]<=arr[st.peek()]){
+                st.pop();
             }
-             if(st1.isEmpty()){
+            if(st.isEmpty()){
                 left[i]= i;
-                st1.push(i);
+                st.push(i);
             }
-            else if (!st1.isEmpty()&& h[st1.peek()]< h[i]){
-                left[i]= i-st1.peek()-1;
-                st1.push(i);
+            else{
+                left[i]= i- st.peek()-1;
+                 st.push(i);
             }
-            i++;
-
-
+          i++;
 
         }
-
-
-        //caculalate max 
         int max=0;
-        for(int j=0;j<n;j++){
-            int c= h[j]* (left[j] + right[j]);
-            max= Math.max(max,c);
 
+        for( i=0;i<n;i++){
+            int area= (left[i]+ right[i])* arr[i];
+            max= Math.max(max, area);
         }
+        return max;
 
- return max;
 
+        
     }
-
 }
