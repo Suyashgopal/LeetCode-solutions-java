68fe9ab8e2254df310ffc6e0270b1956170a6b7f
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-08-03 23:19:57 +0530

    Time: 293 ms (40.74%), Space: 110.3 MB (28.78%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,61 +1,60 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int MOD= 1000000007;
-        Stack<Integer> st= new Stack<>();
-                Stack<Integer> st1= new Stack<>();
-
-
         int n= arr.length;
+        Stack <Integer> st= new Stack<>();
+        int[] left= new int[n];
+        int[] right= new int[n];
 
-       //left to right
-    int i=0;
-    int [] left= new int[n];
-    while(i<n){
-        while(!st.isEmpty() && arr[st.peek()]> arr[i]){
-            st.pop();
-        }
-        if(st.isEmpty()){
-        left[i]=i+1;
-        st.push(i);
-        }
-        else if(!st.isEmpty() && arr[st.peek()]<=arr[i]){
-            left[i]= i-st.peek();
-            st.push(i);
-        }
-        i++;
-    }
+        int mod= (int) 1e9 +7;
+        int i= 0;
 
-      
-       //right to left 
 
-       int j=n-1;
-       int k= n-1; 
-    int [] right= new int[n];
-    while(j>=0){
-        while(!st1.isEmpty() && arr[st1.peek()]>= arr[j]){
-            st1.pop();
+        while(i<n){
+            while(!st.isEmpty() &&  arr[st.peek()]> arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                left[i]= i+1;
+                st.push(i);
+            }
+            else{
+                left[i]= i- st.peek();
+                st.push(i);
+            }
+            i++;
         }
-        if(st1.isEmpty()){
-        right[j]=n-j;
-        st1.push(j);
+
+        st.clear();
+        i=n-1;
+        
+          while(i>=0){
+            while(!st.isEmpty() &&  arr[st.peek()]>=arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                right[i]= n-i;
+                st.push(i);
+            }
+            else{
+                right[i]=  st.peek()-i;
+                st.push(i);
+            }
+            i--;
         }
-        else if(!st.isEmpty() && arr[st1.peek()]<=arr[j]){
-            right[j]= st1.peek()-j;
-            st1.push(j);
-        }
-        j--;
-    }
+     long sum=0;
+     for( i=0; i<n ;i++){
+        long curr= (1L * arr[i]* left[i]* right[i] );
+       sum= (sum+ curr)% mod;
 
-      
-    //calculate sum
+     }
 
-    long sum=0;
-    for(int s=0;s<n;s++){
-        long part = (long)arr[s] * left[s] * right[s];
-        sum= sum + part;
-        sum= sum% MOD;
-    }
-   return (int)sum%MOD;
+     return (int) sum;
 
+
+
+
+
+    
+        
     }
 }
