00a9e9ca861da52b31366d79c6c9b58a53ab0937
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-08-06 23:57:24 +0530

    Time: 536 ms (5.02%), Space: 48.2 MB (52.53%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,134 +1,60 @@
-import java.util.*;
-
 class Solution {
-
     public int leastInterval(char[] arr, int n) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(b[1], a[1]));
+        int k = arr.length;
 
-        // frequency map
         HashMap<Character, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < k; i++) {
             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+
         }
 
-        // max heap array
-        int[] max = new int[map.size()];
+        for (char c : map.keySet()) {
+            int ch = c - 'A';
+            int freq = map.get(c);
+            pq.offer(new int[] { ch, freq, -(n + 1) });
 
-        int ptr = 0;
-        for (int k : map.values()) {
-            max[ptr++] = k;
         }
+                    ArrayList<Integer> al = new ArrayList<>();
 
-        int size = max.length;
 
-        // build heap
-        for (int i = (size / 2) - 1; i >= 0; i--) {
-            downheap(i, max, size);
-        }
 
-        int time = 0;
+        while (!pq.isEmpty()) {
 
-        while (size > 0 && max[0] > 0) {
+            ArrayList<int[]> temp = new ArrayList<>();
+            boolean executed = false;
 
-            ArrayList<Integer> used = new ArrayList<>();
+            while (!pq.isEmpty()) {
 
-            int cycle = n + 1;
-            int workDone = 0;
+                int[] cur = pq.poll();
 
-            for (int i = 0; i < cycle; i++) {
+                if (al.size() >= cur[2] + n + 1) {
 
-                if (size > 0 && max[0] > 0) {
+                    al.add(cur[0]);
+                    cur[1]--;
 
-                    int val = max[0];
-
-                    // remove root
-                    swap(max, 0, size - 1);
-                    size--;
-
-                    if (size > 0) {
-                        downheap(0, max, size);
+                    if (cur[1] > 0) {
+                        cur[2] = al.size() - 1;
+                        pq.offer(cur);
                     }
 
-                    val--;
-
-                    if (val > 0) {
-                        used.add(val);
-                    }
-
-                    workDone++;
-                    time++;
+                    executed = true;
+                    break;
                 }
-                else {
 
-                    // no task left
-                    if (used.size() == 0) {
-                        break;
-                    }
-
-                    // idle time
-                    time++;
-                }
+                temp.add(cur);
             }
 
-            // reinsert used tasks
-            for (int val : used) {
+            for (int[] x : temp)
+                pq.offer(x);
 
-                max[size] = val;
-                int idx = size;
-                size++;
-
-                // upheap
-                while (idx > 0) {
-
-                    int parent = (idx - 1) / 2;
-
-                    if (max[parent] < max[idx]) {
-                        swap(max, parent, idx);
-                        idx = parent;
-                    } else {
-                        break;
-                    }
-                }
+            if (!executed) {
+                al.add(-1); // idle interval
             }
         }
 
-        return time;
-    }
+        return al.size();
 
-    // max heapify
-    void downheap(int i, int[] arr, int size) {
-
-        int idx = i;
-
-        while (idx < size / 2) {
-
-            int left = 2 * idx + 1;
-            int right = 2 * idx + 2;
-
-            int largest = idx;
-
-            if (left < size && arr[left] > arr[largest]) {
-                largest = left;
-            }
-
-            if (right < size && arr[right] > arr[largest]) {
-                largest = right;
-            }
-
-            if (largest != idx) {
-                swap(arr, idx, largest);
-            } else {
-                break;
-            }
-
-            idx = largest;
-        }
-    }
-
-    void swap(int[] arr, int a, int b) {
-
-        int temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
     }
 }
