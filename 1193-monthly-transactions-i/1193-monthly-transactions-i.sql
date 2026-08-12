2d74cda6c49fa47976f83353d39feaa66640b042
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-08-12 22:34:46 +0530

    Time: 735 ms (19.27%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -3,7 +3,7 @@ SELECT
    COUNT(trans_date) as  trans_count ,
    SUM(  state='approved') as  approved_count,
    SUM(amount) as trans_total_amount ,
-SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) as approved_total_amount
+SUM(IF(state = 'approved', amount, 0)) as approved_total_amount 
 FROM Transactions 
 GROUP BY DATE_FORMAT(trans_date , '%Y-%m'), country
 ;
