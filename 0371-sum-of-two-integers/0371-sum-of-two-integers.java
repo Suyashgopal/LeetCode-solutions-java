78c8c8a44ac859c9a380fd15aeef3b95617da4cc
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-08-23 22:03:28 +0530

    Time: 0 ms (100%), Space: 42 MB (52.33%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,41 +1,47 @@
 class Solution {
-    public int getSum(int anum, int bnum) {
+    public int getSum(int a, int b) {
+        int sum=0;
+        int carry=0;
 
-        int carry = 0;
-        int res = 0;
+        for(int i=0;i<32;i++){
+            int abit=  (a>>>i ) & 1;
+            int bbit=  (b>>>i ) & 1;
+          
 
-        for (int i = 0; i < 32; i++) {
+             if(carry==0){
+                if((abit==0 && bbit==1) || (abit==1 && bbit==0)){
+                  sum=   sum |(1<<i);
 
-            int a = anum & 1;
-            int b = bnum & 1;
+                }
+              
+               else if(abit==1 && bbit==1){
+                    carry=1;
+                }
+             }
 
-            if (carry == 0) {
+             else{
+              if((abit==0 && bbit==1) || (abit==1 && bbit==0)){
+                 carry= 1;
 
-                if (a == 1 && b == 1) {
-                    carry = 1;
-                } else if (a != b) {
-                    res |= (1 << i);
+                }
+               else if(abit==0 && bbit==0){
+                   sum= sum | (1<<i);
+                   carry=0;
+                }
+                else{
+                   sum= sum | (1<<i);
+                   carry=1;    
+
                 }
 
-            } else {
 
-                if (a == 0 && b == 0) {
-                    res |= (1 << i);
-                    carry = 0;
-                } else if (a != b) {
-                    // sum = 0, carry = 1
-                    carry = 1;
-                } else { // a == 1 && b == 1
-                    res |= (1 << i);
-                    carry = 1;
-                }
-            }
 
-            // unsigned shift
-            anum >>>= 1;
-            bnum >>>= 1;
+             }
+
+
+        
         }
-
-        return res;
+        return sum;
+        
     }
 }
