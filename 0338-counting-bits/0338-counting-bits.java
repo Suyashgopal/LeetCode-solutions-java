280ae347678c401484fb90720066caaa16df24a6
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-08-25 16:15:50 +0530

    Time: 2 ms (95.79%), Space: 49 MB (14.28%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] arr= new int[n+1];
-        arr[0]=0;
-         if(n==0){
-            return arr;
-         }
-
-         for(int i=1;i<=n;i++){
-            int id= (i& (i-1));
-            arr[i]= arr[id]+1;
-         }
-
-        return arr;
+        int[] res= new int[n+1];
+         res[0]=0;
+        if(n==0){
+           
+            return res;
+        }
+        for(int i=1;i<=n;i++){
+           int id= i& (i-1);
+           res[i]= res[id]+1; 
+        }
+        return res;
+        
     }
 }
