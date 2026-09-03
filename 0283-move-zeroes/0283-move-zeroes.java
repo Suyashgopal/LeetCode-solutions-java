ca0391bd84dc1d69f951b30286d3795c8be29d5a
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-09-03 12:11:04 +0530

    Time: 3 ms (17.86%), Space: 48 MB (20.26%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,20 +1,30 @@
 class Solution {
     public void moveZeroes(int[] arr) {
-        int n= arr.length;
-        int j=0;
+        int n = arr.length;
+        int i=0;
 
-        for(int i=0;i<n;i++){
-            if(arr[i]!=0){
-                swap(i,j,arr);
-                j++;
-            }
+      while(i<n && arr[i]!=0){
+        i++;
+      }
+      int j=i;
+       while(j<n) {
+        if(arr[i]==0 && arr[j]!=0){
+            int temp= arr[i];
+            arr[i]= arr[j];
+            arr[j]= temp;
+            i++;
+           
+
         }
+    
+        
+   
+            j++;
+        
+       }
+
+
 
         
-    }
-    void swap(int i, int j, int[]arr){
-        int temp= arr[i];
-        arr[i]= arr[j];
-        arr[j]=temp;
     }
 }
