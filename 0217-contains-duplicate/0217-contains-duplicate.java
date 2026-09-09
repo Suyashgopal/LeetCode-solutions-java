23557f5aac54683122dd01b3efff29e56dda6542
SUYASH CHAMOLI <suyashchamoli1@gmail.com>
2026-09-09 14:03:31 +0530

    Time: 18 ms (77.06%), Space: 108.3 MB (37.87%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,15 +2,12 @@ class Solution {
     public boolean containsDuplicate(int[] arr) {
         int n= arr.length;
         HashSet<Integer> set= new HashSet<>();
-
         for(int i=0;i<n;i++){
-          if(set.contains(arr[i])){
-            return true;
-          }
-          set.add(arr[i]);
+      if(set.contains(arr[i])){
+        return true;
+      }
+      set.add(arr[i]);
         }
-
-return false;
-        
+        return false;
     }
 }
